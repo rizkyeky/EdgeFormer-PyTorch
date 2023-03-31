3ca64abd016010a38c605ebfb9e93addc1abfdc6
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2023-04-01 06:23:05 +0700

    update

--- export_tensorrt.py
+++ export_tensorrt.py
@@ -1,16 +1,16 @@
 import torch
 import torchvision
 import torch.nn as nn
-import torch_tensorrt as torchtrt
+# import torch_tensorrt as torchtrt
 import torch_tensorrt
 
 model = torchvision.models.mobilenet_v3_small(weights="IMAGENET1K_V1").cuda().half().eval()
 
 inputs = [
     torch_tensorrt.Input(
-        min_shape=[1, 1, 16, 16],
-        opt_shape=[1, 1, 32, 32],
-        max_shape=[1, 1, 64, 64],
+        min_shape=[1, 1, 64, 64],
+        opt_shape=[1, 2, 128, 128],
+        max_shape=[1, 3, 256, 256],
         dtype=torch.half,
     )
 ]
@@ -20,6 +20,6 @@ trt_ts_module = torch_tensorrt.compile(
     model, inputs=inputs, enabled_precisions=enabled_precisions
 )
 input_data = torch.randn((1,3,224,224))
-input_data = input_data.to("cuda").half()
+input_data = input_data.to(torch.device("cuda")).half()
 result = trt_ts_module(input_data)
 torch.jit.save(trt_ts_module, "mobilenet_v3_small.trt")
