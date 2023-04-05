547d613c87aef8a9d7752755dc21d78155643460
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2023-04-05 09:01:27 +0700

    update

--- show_history.py
+++ show_history.py
@@ -1,14 +1,23 @@
 import matplotlib.pyplot as plt
 import json
 import sys
- 
+import numpy as np
+
 with open('results/run10000/{}'.format(sys.argv[1]), 'r') as f:
     history = json.load(f)
 
-plt.plot(history['train_avg_loss'], label='Training Loss')
-plt.plot(history['val_avg_loss'], label='Validation Loss')
+
+train_loss = history['train_avg_loss']
+val_loss = history['val_avg_loss']
+
+print(train_loss[0:10], train_loss[-10:])
+
+plt.plot(train_loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
+plt.xticks(np.arange(0,len(history['train_avg_loss'])+1,500))
 plt.legend()
 
 plt.show()
+
