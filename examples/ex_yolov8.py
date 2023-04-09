7c1cdc122406a324d514fb4a40ad188edd90fb95
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2023-04-09 21:01:51 +0700

    update

---
+++ examples/ex_yolov8.py
@@ -0,0 +1,10 @@
+import torch
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    IMG_SIZE = 224
+
+    model = YOLO('yolov8m.pt')
+    model.to(device)
+    model.export(format='torchscript', imgsz=IMG_SIZE, half=True)
