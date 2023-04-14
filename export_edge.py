a925a51bcd72fb309e93225c413d7a7035b3c856
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2023-04-14 09:38:52 +0700

    update

--- export_edge.py
+++ export_edge.py
@@ -4,9 +4,9 @@ import main_det
 
 if __name__ == '__main__':
 
-    model = main_det.init_model()
+    model = main_det.init_model().cuda()
     
-    dummy_input = torch.rand(1, 3, 224, 224)
+    # dummy_input = torch.rand(1, 3, 224, 224)
     model_traced = torch.jit.trace(model)
     model_scripted = torch.jit.script(model_traced)
 
