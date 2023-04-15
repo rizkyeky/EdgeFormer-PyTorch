c73d3166f43ee937b5327499c4edf37f6445f8d9
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2023-04-15 11:37:49 +0700

    update

--- export_edge.py
+++ export_edge.py
@@ -4,11 +4,18 @@ import main_det
 
 if __name__ == '__main__':
 
-    model = main_det.init_model().cuda()
+    model = main_det.init_model()
     
-    # dummy_input = torch.rand(1, 3, 224, 224)
-    model_traced = torch.jit.trace(model)
-    model_scripted = torch.jit.script(model_traced)
+    dummy_input = torch.rand(1, 3, 224, 224)
+    try:
+        model_traced = torch.jit.trace(model, dummy_input)
+    except Exception as e:
+        err = e.__str__().split('\n')
+        print(len(err))
+        print(err[:10])
+        # print('Error tracing model')
+        exit()
+    # model_scripted = torch.jit.script(model)
 
     # model_optimized = optimize_for_mobile(model_scripted)
-    model_scripted.save('pretrained/edgeformer-det.pt')
+    # model_scripted.save('pretrained/edgeformer-det.pt')
