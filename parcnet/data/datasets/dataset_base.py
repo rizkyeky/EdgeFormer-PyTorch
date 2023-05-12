fce557c6383ab39f426c5c8efae0589480f5abad
rizkyeky <mochamad.rizky.darmawan@gmail.com>
2023-05-12 20:16:44 +0700

    update

--- parcnet/data/datasets/dataset_base.py
+++ parcnet/data/datasets/dataset_base.py
@@ -22,6 +22,7 @@ class BaseImageDataset(data.Dataset):
         if (system == 'labai'):
             self.root = root[1:]
         else:
+            root = root+'dataset/'
             self.root = root + 'imbalance' if self.is_imbalance else root + 'balance'
         self.is_training = is_training
         self.is_evaluation = is_evaluation
